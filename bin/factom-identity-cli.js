21d73bfd72461ac8c0e13697367d8a3a5c74eefc
Paul <bernier.pja@gmail.com>
2018-05-03 09:41:55 -0700

    Display error and help for un recognize command

--- bin/factom-identity-cli.js
+++ bin/factom-identity-cli.js
@@ -8,3 +8,9 @@ program
     .command('update-coinbase-address', 'Update coinbase address')
     .command('update-efficiency', 'Update efficiency')
     .parse(process.argv);
+
+if (!['get', 'update-coinbase-address', 'update-efficiency'].includes(program.args[0])) {
+    console.error(`Invalid command [${program.args[0]}]`);
+    program.outputHelp();
+    process.exit(1);
+}
